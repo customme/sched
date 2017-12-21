f1fc08495c347bfaa1a0a85bf017e24565b82adf
customme@163.com <customme@163.com>
2017-12-21 12:34:37 +0800

    每天一个任务改成时段任务

--- sched-core/shell/test/task_controller.sh
+++ sched-core/shell/test/task_controller.sh
@@ -16,7 +16,7 @@ CHECK_INTERVAL=60
 # 数据文件目录
 DATADIR=/work/data/logs
 # 日志文件目录
-LOGDIR=/work/www/shell/logs
+LOGDIR=./logs
 
 
 # 创建目录
@@ -41,23 +41,29 @@ function exec_sql()
 function get_tasks()
 {
     echo "SELECT
-      prod_id,
+      a.prod_id,
       prod_name,
-      MIN(run_date),
+      start_date,
+      end_date,
+      run_status,
       IF(new_time > 0, 0, 1),
       IF(active_time > 0, 0, 1),
       IF(visit_time > 0, 0, 1),
       IF(report_time > 0, 0, 1)
-    FROM t_prod_run
-    WHERE run_status = 1
-    GROUP BY prod_id;
+    FROM t_prod_run a
+    INNER JOIN (
+      SELECT prod_id, MIN(start_date) min_date FROM t_prod_run WHERE run_status <> 3 GROUP BY prod_id 
+    ) b
+    ON a.prod_id = b.prod_id
+    AND start_date = min_date
+    AND run_status <> 3;
     " | exec_sql
 }
 
 # 获取新增量
 function get_new_cnt()
 {
-    echo "SELECT IFNULL(SUM(adduser), 0) FROM l_all_add WHERE stattime = '$run_date' AND proname = '$prod_name';" | exec_sql
+    echo "SELECT IFNULL(SUM(adduser), 0) FROM l_all_add WHERE stattime >= '$start_date' AND stattime <= '$end_date' AND proname = '$prod_name';" | exec_sql
 }
 
 # 更新任务状态
@@ -65,7 +71,7 @@ function update_task()
 {
     local updates="$1"
     local filters="$2"
-    local sql="UPDATE t_prod_run SET $updates WHERE prod_id = '$prod_id' AND run_date = '$run_date' $filters;"
+    local sql="UPDATE t_prod_run SET $updates WHERE prod_id = '$prod_id' AND start_date = '$start_date' AND end_date = '$end_date' $filters;"
 
     log "$sql"
     echo "$sql" | exec_sql
@@ -75,7 +81,7 @@ function update_task()
 function gen_visit()
 {
     # 生成访问
-    sh gen_data3-1.sh -b $prod_id,$run_date,$run_date > $LOGDIR/${prod_id}.${run_date}.log.3 2> $LOGDIR/${prod_id}.${run_date}.err.3
+    sh gen_data3-1.sh -b $prod_id,$start_date,$end_date > $LOGDIR/${prod_id}.${start_date}.log.4 2> $LOGDIR/${prod_id}.${start_date}.err.4
     if [[ $? -eq 0 ]]; then
         # 更新访问生成时间
         update_task "visit_time = NOW()"
@@ -83,7 +89,7 @@ function gen_visit()
         # 更新任务状态
         update_task "run_status = 3" "AND visit_time > 0 AND report_time > 0"
     else
-        error_msg=`sed "s/\('\|\"\)/\\\\\1/g" $LOGDIR/${prod_id}.${run_date}.err.3 | awk '{printf("%s\\\n",$0)}'`
+        error_msg=`sed "s/\('\|\"\)/\\\\\1/g" $LOGDIR/${prod_id}.${start_date}.err.4 | awk '{printf("%s\\\n",$0)}'`
         update_task "run_status = 4, error_msg = CONCAT(IFNULL(error_msg,''), '\nGenerate visit failed\n', '$error_msg')"
     fi
 }
@@ -98,10 +104,11 @@ function run()
 
         # 检查生成android id的锁是否存在
         while test -f $DATADIR/aid.lock; do
+            log "Wait for lock ($prod_id, $start_date $end_date)"
             sleep 1
         done
         # 创建锁
-        log "Create lock ($prod_id, $run_date)"
+        log "Create lock ($prod_id, $start_date $end_date)"
         touch $DATADIR/aid.lock
 
         # 获取android id个数
@@ -122,9 +129,9 @@ function run()
         diff=$((max_id + new_cnt - aid_cnt))
         if [[ $diff -gt 0 ]]; then
             # 生成android id
-            sh gen_data3-1.sh -a $diff > $LOGDIR/aid.log 2> $LOGDIR/aid.err
+            sh gen_data3-1.sh -a $diff > $LOGDIR/${prod_id}.${start_date}.log.1 2> $LOGDIR/${prod_id}.${start_date}.err.1
             if [[ $? -gt 0 ]]; then
-                error_msg=`sed "s/\('\|\"\)/\\\\\1/g" $LOGDIR/aid.err | awk '{printf("%s\\\n",$0)}'`
+                error_msg=`sed "s/\('\|\"\)/\\\\\1/g" $LOGDIR/${prod_id}.${start_date}.err.1 | awk '{printf("%s\\\n",$0)}'`
                 update_task "run_status = 4, error_msg = CONCAT(IFNULL(error_msg,''), '\nGenerate android id failed\n', '$error_msg')"
 
                 # 出错退出
@@ -133,12 +140,12 @@ function run()
         fi
 
         # 生成新增
-        sh gen_data3.sh -a $prod_id,$run_date,$run_date > $LOGDIR/${prod_id}.log.1 2> $LOGDIR/${prod_id}.err.1
+        sh gen_data3.sh -a $prod_id,$start_date,$end_date > $LOGDIR/${prod_id}.${start_date}.log.2 2> $LOGDIR/${prod_id}.${start_date}.err.2
         if [[ $? -eq 0 ]]; then
             # 更新新增生成时间
             update_task "new_time = NOW()"
         else
-            error_msg=`sed "s/\('\|\"\)/\\\\\1/g" $LOGDIR/${prod_id}.err.1 | awk '{printf("%s\\\n",$0)}'`
+            error_msg=`sed "s/\('\|\"\)/\\\\\1/g" $LOGDIR/${prod_id}.${start_date}.err.2 | awk '{printf("%s\\\n",$0)}'`
             update_task "run_status = 4, error_msg = CONCAT(IFNULL(error_msg,''), '\nGenerate new failed\n', '$error_msg')"
 
             # 出错退出
@@ -146,19 +153,19 @@ function run()
         fi
 
         # 释放锁
-        log "Release lock ($prod_id, $run_date)"
+        log "Release lock ($prod_id, $start_date $end_date)"
         rm -f $DATADIR/aid.lock
     fi
 
     # 活跃
     if [[ $active_status -eq 1 ]]; then
         # 生成活跃
-        sh gen_data3.sh -b $prod_id,$run_date,$run_date > $LOGDIR/${prod_id}.${run_date}.log.2 2> $LOGDIR/${prod_id}.${run_date}.err.2
+        sh gen_data3.sh -b $prod_id,$start_date,$end_date > $LOGDIR/${prod_id}.${start_date}.log.3 2> $LOGDIR/${prod_id}.${start_date}.err.3
         if [[ $? -eq 0 ]]; then
             # 更新活跃生成时间
             update_task "active_time = NOW()"
         else
-            error_msg=`sed "s/\('\|\"\)/\\\\\1/g" $LOGDIR/${prod_id}.${run_date}.err.2 | awk '{printf("%s\\\n",$0)}'`
+            error_msg=`sed "s/\('\|\"\)/\\\\\1/g" $LOGDIR/${prod_id}.${start_date}.err.3 | awk '{printf("%s\\\n",$0)}'`
             update_task "run_status = 4, error_msg = CONCAT(IFNULL(error_msg,''), '\nGenerate active failed\n', '$error_msg')"
 
             # 出错退出
@@ -167,21 +174,21 @@ function run()
     fi
 
     # 访问
-    if [[ $visit_status -eq 1 && ! `ps aux | grep "gen_data3-1\.sh -b $prod_id,$run_date,$run_date"` ]]; then
+    if [[ $visit_status -eq 1 && ! `ps aux | grep "gen_data3-1\.sh -b $prod_id,$start_date,$end_date"` ]]; then
         gen_visit &
     fi
 
     # 报表
-    if [[ $report_status -eq 1 && ! `ps aux | grep "sh load_active3\.sh $prod_id,$run_date,$run_date"` ]]; then
+    if [[ $report_status -eq 1 && ! `ps aux | grep "sh load_active3\.sh $prod_id,$start_date,$end_date"` ]]; then
         # 统计报表
-        sh load_active3.sh $prod_id $run_date $run_date > $LOGDIR/${prod_id}.${run_date}.log.4 2> $LOGDIR/${prod_id}.${run_date}.err.4
+        sh load_active3.sh $prod_id $start_date $end_date > $LOGDIR/${prod_id}.${start_date}.log.5 2> $LOGDIR/${prod_id}.${start_date}.err.5
         if [[ $? -eq 0 ]]; then
             # 更新报表生成时间
             update_task "report_time = NOW()"
             # 更新任务状态
             update_task "run_status = 3" "AND visit_time > 0 AND report_time > 0"
         else
-            error_msg=`sed "s/\('\|\"\)/\\\\\1/g" $LOGDIR/${prod_id}.${run_date}.err.4 | awk '{printf("%s\\\n",$0)}'`
+            error_msg=`sed "s/\('\|\"\)/\\\\\1/g" $LOGDIR/${prod_id}.${start_date}.err.5 | awk '{printf("%s\\\n",$0)}'`
             update_task "run_status = 4, error_msg = CONCAT(IFNULL(error_msg,''), '\nGenerate report failed\n', '$error_msg')"
         fi
     fi
@@ -190,15 +197,19 @@ function run()
 # 检查可执行任务
 function check()
 {
-    get_tasks | while read prod_id prod_name run_date new_status active_status visit_status report_status; do
-        any_status=$((new_status + active_status + visit_status + report_status))
-        if [[ $any_status -eq 0 ]]; then
-            log "There is no task to run"
-            update_task "run_status = 3"
+    get_tasks | while read prod_id prod_name start_date end_date run_status new_status active_status visit_status report_status; do
+        if [[ $run_status -eq 1 ]]; then
+            any_status=$((new_status + active_status + visit_status + report_status))
+            if [[ $any_status -eq 0 ]]; then
+                log "The task($prod_id, $start_date, $end_date) is already done, now to update the task status"
+                update_task "run_status = 3"
+            else
+                log "Run task($prod_id, $start_date, $end_date)"
+                update_task "run_status = 2"
+                run &
+            fi
         else
-            log "Run task: $prod_id $run_date"
-            update_task "run_status = 2"
-            run &
+            log "The task($prod_id, $start_date, $end_date, $run_status) is not ready yet"
         fi
     done
 }
