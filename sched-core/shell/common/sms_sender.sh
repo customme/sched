abd6b03b342e5cbdb9aeb760980dbe4d76600b1f
customme@163.com <customme@163.com>
2018-02-03 15:34:54 +0800

    修改短信接口发送内容过长失败的bug

--- sched-core/shell/common/sms_sender.sh
+++ sched-core/shell/common/sms_sender.sh
@@ -40,42 +40,41 @@ function main()
 
     # 超出单条短信内容最大长度，则拆分成多条
     echo -e "$msg_content" | awk '{
-        total=total + length($0)
+        len = length($0)
+        total += len
         if(total > max_length){
             if(msg > ""){
-                print msg > "'$TMP_DIR/$no/message-'"++j
-                total=length($0)
-                msg=$0
-            }else{
-                size=int(length($0) / max_length)
-                mod=length($0) % max_length
-                if (mod > 0) size = size + 1
+                ptotal = total - len
+                size = int(ptotal / max_length)
+                if (ptotal % max_length > 0) size += 1
                 for(i=0;i<size;i++){
-                    print substr($0,i * max_length + 1,max_length) > "'$TMP_DIR/$no/message-'"++j
+                    print substr(msg,i * max_length + 1,max_length) > "'$TMP_DIR/$no/message-'"++j
                 }
             }
+            total = len
+            msg = $0
         }else{
             if(msg > ""){
-                msg=msg"\n"$0
+                msg = msg"\n"$0
             }else{
-                msg=$0
+                msg = $0
             }
         }
     } END {
-        size=int(length(msg) / max_length)
-        mod=length(msg) % max_length
-        if (mod > 0) size = size + 1
+        size = int(total / max_length)
+        if (total % max_length > 0) size += 1
         for(i=0;i<size;i++){
             print substr(msg,i * max_length + 1,max_length) > "'$TMP_DIR/$no/message-'"++j
         }
     }' max_length=$MAX_LENGTH
 
     # 发送短信
-    ls -c $TMP_DIR/$no/message-* | while read file_name; do
+    ls $TMP_DIR/$no/message-* | sort -t '-' -k 2n | while read file_name; do
         # 短信内容
         post_data="content=九指运维短信报警：`cat $file_name`【牙牙关注】"
 
-        curl -s -w "\n%{http_code} %{time_connect} %{time_starttransfer} %{time_total}\n" --connect-timeout $TIMEOUT --data-urlencode "$post_data" "${SMS_URL}?action=send&account=${ACCOUNT}&password=${PASSWORD}&mobile=${phone_nums}" >> $TMP_DIR/$no/sms_log 2>&1
-    done
+        curl -s -w "\n%{http_code} %{time_connect} %{time_starttransfer} %{time_total}\n" --connect-timeout $TIMEOUT \
+--data-urlencode "$post_data" "${SMS_URL}?action=send&account=${ACCOUNT}&password=${PASSWORD}&mobile=${phone_nums}"
+    done > $TMP_DIR/$no/sms_log 2>&1
 }
 main "$@"
